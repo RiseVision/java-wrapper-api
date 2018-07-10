623cabf9b49c54dc307f7e8fa00d9590f0c60c2e
Andrea Baccega <vekexasia@gmail.com>
2018-07-10 10:05:17 +0200

    Added javadoc and sources to artifacts.

--- src/main/java/rise/vision/apis/Blocks.java
+++ src/main/java/rise/vision/apis/Blocks.java
@@ -12,22 +12,36 @@ import java.util.Map;
  */
 public interface Blocks {
 
+  /**
+   * Gets current fee structure
+   */
   @GET("/api/blocks/getFees")
   Call<FeesResponse> getAllFees();
 
+  /**
+   * Fetches current blocks status
+   */
   @GET("/api/blocks/getStatus")
   Call<StatusResponse> getStatus();
 
+  /**
+   * Gets current height
+   */
   @GET("/api/blocks/getHeight")
   Call<HeightResponse> getHeight();
 
   /**
    * Use #BlocksQueryBuilder to provide parameter here
+   * @param query The Query map. Use {@link rise.vision.apis.utils.BlocksQueryBuilder}
    */
   @GET("/api/blocks/")
   Call<BlocksResponse> getBlocks(@QueryMap Map<String, String> query);
 
 
+  /**
+   * Query and gets a single block
+   * @param id block id to query
+   */
   @GET("/api/blocks/get")
   Call<BlockResponse> getBlock(@Query("id") String id);
 
